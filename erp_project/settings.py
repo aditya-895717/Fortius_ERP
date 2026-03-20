e31ba2eb9c2d089a535fc68ef3bc1e7c58f548e6
Aditya Dubey <166470390+aditya-895717@users.noreply.github.com>
2026-03-20 16:08:23 +0530

    New Update babu

--- erp_project/settings.py
+++ erp_project/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -109,9 +110,15 @@ USE_TZ = True
 # ---------------------------------------------------------------------------
 # Static and Media files
 # ---------------------------------------------------------------------------
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Use WhiteNoise's manifest storage to serve static files in production
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
